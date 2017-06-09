f02c46fe6f48e2274e355fd5e997f36f8b57765e
Isak La Fleur <isak.engdahl@gmail.com>
2017-06-09 13:35:09 +0200

    added bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <button type="button" className="btn btn-primary">Primary</button>
       </div>
     );
   }
